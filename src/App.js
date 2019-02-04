ca312e8286e4817bd45a842064a4aaa0c555d1b3
bluesophia <imsophia0313@gmail.com>
2019-02-04 16:19:44 +1300

    arrange size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import CardInfobox from './Components/CardInfobox/CardInfobox'
-import './App.css';
+import CardInfobox from './Components/CardInfobox/CardInfobox';
+import CardWeatherBox from './Components/CardWeatherbox/CardWeatherbox';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CardInfobox />
+        <CardWeatherBox />
       </div>
     );
   }
