6831b55c42d1be7dfc12d9dbfbffbb656a946afc
bluesophia <imsophia0313@gmail.com>
2019-02-14 18:28:24 +1300

    update alert with img

--- src/Components/CardAlertwithImg/CardAlertwithImg.js
+++ src/Components/CardAlertwithImg/CardAlertwithImg.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './CardAlertDissmissal.css'
-import { AlertDissmissal } from '../Common/Alert/AlertExports';
+import './CardAlertwithImg.css'
+import { AlertImgDissmissal } from '../Common/Alert/AlertExports';
 
-class CardAlertDissmissal extends Component {
+class CardAlertwithImg extends Component {
     constructor(props){
         super(props);
         const AlertDissmissalList=[
@@ -13,17 +13,20 @@ class CardAlertDissmissal extends Component {
                id:1,
                backgroundColor: "#fae1e4",
                border: "1px solid #f7d5d9",
+               src: "http://eliteadmin.themedesigner.in/demos/bt4/assets/images/users/1.jpg",
                color: "#77373d"
             },
             {
                id:2,
                backgroundColor: "#fff3cd",
+               src: "http://eliteadmin.themedesigner.in/demos/bt4/assets/images/users/2.jpg",
                border: "1px solid #ffeeba",
                color: "#846404"
             },
             {
                id:3,
                backgroundColor: "#cdeefd",
+               src: "http://eliteadmin.themedesigner.in/demos/bt4/assets/images/users/3.jpg",
                border: "1px solid #b8e7fc",
                color: "#02587e"
             }
@@ -50,13 +53,14 @@ class CardAlertDissmissal extends Component {
             <div className="row col-lg-6">
                 <div className="alert-card">
                     <div className="alert-card-body">
-                        <h3>Dissmissal Alert</h3>
+                        <h3>Alert with image / icon</h3>
                             <div className="alert-card-toaster-btn">
                                 {
                                     AlertDissmissalList.map((item, index)=> (
-                                        <AlertDissmissal
+                                        <AlertImgDissmissal
                                             onClick={this.removeCard}
                                             key={item.id}
+                                            src={item.src}
                                             backgroundColor={item.backgroundColor}
                                             border={item.border}
                                             color={item.color}
@@ -70,4 +74,4 @@ class CardAlertDissmissal extends Component {
             )
         }   
     }
-export default CardAlertDissmissal;
+export default CardAlertwithImg;
--- src/Components/Common/Alert/AlertExports.js
+++ src/Components/Common/Alert/AlertExports.js
@@ -1,2 +1,3 @@
 export { default as AlertDefault } from './AlertDefault/AlertDefault';
 export { default as AlertDissmissal } from './AlertDissmissal/AlertDissmissal';
+export { default as AlertImgDissmissal } from './AlertImgDissmissal/AlertImgDissmissal';
