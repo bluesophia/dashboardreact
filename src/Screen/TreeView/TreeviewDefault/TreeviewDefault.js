9dc12cfc0fa42e43fea7ee8336db77a500701bb8
bluesophia <imsophia0313@gmail.com>
2019-02-21 12:47:58 +1300

    create treeview

--- src/Screen/TreeView/TreeviewDefault/TreeviewDefault.js
+++ src/Screen/TreeView/TreeviewDefault/TreeviewDefault.js
@@ -1,134 +1,132 @@
 import React, { Component } from 'react';
-import './TreeviewDefault.css'
-import CardTitlewithOptions from '../../../Components/CardTitlewithOptions/CardTitlewithOptions';
-import CardWidgetWeatherTable from '../../../Components/CardWidgetWeatherTable/CardWidgetWeatherTable';
-import WeatherIcons from 'react-weathericons';
+import './TreeviewDefault.css';
+import TreeView from 'react-treeview';
 import '../../../Assets/Styles/weather-icons.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Plus from '../../../Assets/Images/plus-black.svg';
+import Minus from '../../../Assets/Images/minus-black.svg';
 
+// treevew default list
+const treeviewDefaultList = [
+    {
+        type: 'Parent 1',
+        collapsed: false,
+        children: [
+            { name: 'child 1', detail1: 'Grandchild 1', detail2: 'Grandchild 2', collapsed: false},
+            { name: 'child 2', collapsed: false},
+        ]
+    },
+    {
+        type: 'Parent 2',
+        collapsed: false,
+        children: [],
+    },
+    {
+        type: 'Parent 3',
+        collapsed: false,
+        children: [],
+    },
+    {
+        type: 'Parent 4',
+        collapsed: false,
+        children: [],
+    },
+    {
+        type: 'Parent 5',
+        collapsed: false,
+        children: [],
+    },
+  ];
 
 class TreeviewDefault extends Component {
- render(){
-    const tableList = [
-        {
-            id: 0,
-            name:'Wind',
-            value:'ESE 17 mph'
-        },
-        {
-            id: 1,
-            name:'Humidity',
-            value:'83%'
-        },
-        {
-            id: 2,
-            name:'Pressure',
-            value:'28.56 in'
-        },
-        {
-            id: 3,
-            name:'Cloud Cover',
-            value:'78%'
-        },
-        {
-            id: 4,
-            name:'Ceiling',
-            value:'25760 ft'
-        },
-    ]
-    // weather select option list
-    const weatherList = [
-        {
-            id:0,
-            name: 'Today'
-        },
-        {
-            id:1,
-            name: 'Weekly'
+    constructor(props){
+        super(props);
+        this.state = {
+            foldercollapsed: treeviewDefaultList.map(() => false),
         }
-    ]
-    // weather footer list
-    const weatherFooter = [
-        {
-            id: 0,
-            icon: 'day-sunny',
-            time: '09:30',
-            degree: '70'
-        },
-        {
-            id: 1,
-            icon: 'day-cloudy',
-            time: '11:30',
-            degree: '72'
-        },
-        {
-            id: 2,
-            icon: 'day-hail',
-            time: '13:30',
-            degree: '75'
-        },
-        {
-            id: 3,
-            icon: 'day-sprinkle',
-            time: '15:30',
-            degree: '76'
-        },
-    ]
+        this.handleToggle = this.handleToggle.bind(this);
+
+    }
+
+    handleToggle(i) {
+        
+        let [...foldercollapsed] = this.state.foldercollapsed;
+        foldercollapsed[i] = !foldercollapsed[i];
+        this.setState({
+            foldercollapsed: foldercollapsed
+        })
+    }
+
+    render(){
+
+        const foldercollapsed = this.state.foldercollapsed;
+
      return(
-        <div className="col-lg-6">
-        <div className="widgetweather-card">
-
-            {/* weather title */}
-            <div className="widgetweather-card-body">
-                <div className="widgetweather-table">
-                    <CardTitlewithOptions 
-                        titleName="Weather Report"
-                        optionList={weatherList}
-                    />
-                </div> 
-            </div>
-
-            {/* weather icon */}
-            <div className="widgetweather-card-icon-body">
-                <div className="widgetweather-weather">
-                    <div className="widgetweather-weather-icon">
-                        <h1 className="widgetweather-weather-icon-wi">
-                            <WeatherIcons name="day-showers" size="lg" style={WeatherIconsStyle} />
-                        </h1>
-                        <h1 className="widgetweather-weather-icon-h1">73</h1>
-                        <sup className="widgetweather-weather-icon-sup">°</sup>
-                    </div>
-                    <div className="widgetweather-weather-area">
-                        <h3>Saturday</h3>
-                        <small>Ahmedabad, India</small>
-                    </div>
+        <div className="treeviewdefault-row">
+        <div className="treeviewdefault-col-lg-6">
+            <div className="treeviewdefault-card">
+                <div className="treeviewdefault-card-body">
+                    <h5 className="treeviewdefault-card-title">TO DO LIST</h5>
                 </div>
+                <div className="treeviewdefault-tree">
+                    {/* data mapping */}
+                    { treeviewDefaultList.map((item, i) => {
 
-                {/* weather table */}
-                <div className="projectomonth-table-responsive">
-                    <table className="projectomonth-table">
-                        <tbody>
-                        {
-                            tableList.map((item, index) => (
-                                <CardWidgetWeatherTable 
-                                    key={index}
-                                    name={item.name}
-                                    value={item.value}
-                                />
-                            ))
-                        }
-                        </tbody>
-                    </table>
-                </div>  
-                <hr className="projectomonth-hr"/>
+                        // Parent folder
+                        const label = 
+                            <span className="treeviewdefault-parent" onClick={this.handleToggle.bind(null, i)}>
+                                { foldercollapsed ? 
+                                    <img src={Plus} style={{ width: '10px', marginRight: '10px' }}/> 
+                                    :  
+                                    <img src={Minus} style={{ width: '10px', marginRight: '10px' }}/> 
+                                } 
+                                <FontAwesomeIcon icon={faFolder} style={FontAwesomeIconStyle}/>
+                                Parent {i + 1}
+                            </span>;
+
+                        return (
+                            <TreeView
+                            className="treeviewdefault-treeview"
+                            key={i}
+                            nodeLabel={ label ? label : null }
+                            defaultCollapsed={false}
+                            collapsed={foldercollapsed[i]}
+                            >
+                            {/* Children folder */}
+                            {item.children.map(child => {
+                                const label2 = <span className="treeviewdefault-children-name"><FontAwesomeIcon icon={faFolder} style={FontAwesomeIconStyle}/>{child.name}</span>;
+                                return(
+                                    // grandchildren folder
+                                    <TreeView nodeLabel={ label2 ? label2 : null } key={child.name} defaultCollapsed={false} onClick={this.handleToggle.bind(null, i)}>
+                                        { child.detail1 ? 
+                                            <div className="treeviewdefault-treeview-grandchildren">
+                                                <FontAwesomeIcon icon={faFolder} style={FontAwesomeIconStyle}/>
+                                                {child.detail1}
+                                            </div> 
+                                        : null }
+                                        { child.detail2 ? 
+                                            <div className="treeviewdefault-treeview-grandchildren">
+                                                <FontAwesomeIcon icon={faFolder} style={FontAwesomeIconStyle}/>
+                                                    {child.detail2}
+                                            </div> 
+                                        : null }
+                                    </TreeView>
+                                )
+                            })}
+                            </TreeView>
+                        );
+                    })}
+                </div>
             </div>
         </div>
-        </div>
+    </div>  
+    
         )
     }   
 }
 
-const WeatherIconsStyle = {
-    marginLeft: "10px"
+const FontAwesomeIconStyle = {
+    marginRight: '10px'
 }
-
 export default TreeviewDefault;
