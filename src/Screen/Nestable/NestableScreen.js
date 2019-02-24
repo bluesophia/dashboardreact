bc857fd7bf22d5bded855f5501f571d076f2092f
bluesophia <imsophia0313@gmail.com>
2019-02-25 11:09:50 +1300

    add media grid

--- src/Screen/Nestable/NestableScreen.js
+++ src/Screen/Nestable/NestableScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Nestable1, Nestable2, Nestable3 } from './NestableExports.js';
+import './NestableScreen.css';
 
 class NestableScreen extends Component {
     render(){
