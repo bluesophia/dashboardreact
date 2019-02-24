cc4203b50118da0ef4853173f81b413ec11c93a7
bluesophia <imsophia0313@gmail.com>
2019-02-25 10:33:24 +1300

    create nestable2

---
+++ src/Screen/Nestable/Nestable2/Nestable2.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './Nestable2.css';
+import Nestable from 'react-nestable';
+
+// Nestable12Listist => id must be unique    
+const Nestable2tList = [
+    {   
+        id: 0,
+        text: 'item 13'
+    },
+    {   
+        id: 1,
+        text: 'item 14'
+    },
+    {   
+        id: 3,
+        text: 'item 16'
+    },
+    {   
+        id: 4,
+        text: 'item 17'
+    },
+    {   
+        id: 5,
+        text: 'item 18'
+    },
+    {   
+        id: 6,
+        text: 'item 19'
+    },
+    {
+        id:2,
+        text: 'item 15',
+        children: [
+            { id:2, text: 'item 16' },
+            { id:3, text: 'item 17' },
+            { id:4, text: 'item 18' }
+        ]
+    },
+  ];
+      
+    // renderItem  
+    const renderItem = ({ item, collapseIcon, handler }) => {
+        return (
+          <div>
+            {handler}
+            {collapseIcon}
+            {item.text}
+          </div>
+        );
+    };
+    // renderIcon for collapsed
+    // const renderIcon = ({ isCollapsed  }) => {
+    //     <DefaultIcon />
+    // }
+
+class Nestable2 extends Component {
+    render(){
+        return(
+            <div className="nestable2-row">
+                <div className="nestable2-col-lg-6">
+                    <div className="nestable2-card">
+                        <div className="nestable2-card-body">
+                            <h5 className="nestable2-card-title">Nestable2</h5>
+                        </div>
+                        <div>
+                        <div>
+                        <Nestable
+                            collapsed={false}
+                            items={Nestable2tList}
+                            renderItem={renderItem}
+                            // collapseIcon={collapseIcon}
+                            // className="nestable"
+                        />
+                        </div>   
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        )
+    }   
+}
+export default Nestable2;
--- src/Screen/Nestable/NestableExports.js
+++ src/Screen/Nestable/NestableExports.js
@@ -1,2 +1,3 @@
 export { default as Nestable1 } from './Nestable1/Nestable1';
+export { default as Nestable2 } from './Nestable2/Nestable2';
 
