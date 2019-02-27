588589200785eca8c5c43c24782896e0ffaea57f
bluesophia <imsophia0313@gmail.com>
2019-02-27 15:56:51 +1300

    create treeview bluetheme

--- src/Screen/TreeView/TreeViewExports.js
+++ src/Screen/TreeView/TreeViewExports.js
@@ -1,2 +1,4 @@
 export { default as TreeviewDefault } from './TreeviewDefault/TreeviewDefault';
-
+export { default as TreeviewCollapsed } from './TreeviewCollapsed/TreeviewCollapsed';
+export { default as TreeviewTagedasBadges } from './TreeviewTagedasBadges/TreeviewTagedasBadges';
+export { default as TreeviewBlueTheme } from './TreeviewBlueTheme/TreeviewBlueTheme';
