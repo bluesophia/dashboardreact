206e1c5347f99f3be59468c02143493f6e5a6b60
bluesophia <imsophia0313@gmail.com>
2019-02-28 15:53:32 +1300

    login invalid red error sign

--- src/Components/Common/Input/InputPassword/InputPassword.js
+++ src/Components/Common/Input/InputPassword/InputPassword.js
@@ -4,48 +4,39 @@ import './InputPassword.css';
 class InputPassword extends Component {
   constructor(props){
     super(props);
-    this.state = {value: ''};
-    this.handleUserPasswordInput = this.handleUserPasswordInput.bind(this);
-  }
-
-  handleUserPasswordInput(e) {
-    const password = e.target.password;
-    const value = e.target.value;
-    this.setState(
-      {[password]: value},
-      () => { this.validateField(password, value)}
-      );
-    // this.setState({value: event.target.value});
   }
   
   render() {
+      const { value, onChange, onBlur, borderBottom} = this.props;
     return (
         <div className="inputPassword-container">
             <input 
-            style={InputStyle}
+            style={
+              {
+                boxSizing: "border-box",
+                width: "100%",
+                height: "30px",
+                background: "none",
+                borderRadius: 0,
+                border: 0,
+                borderBottom: borderBottom,
+                color: "#6c757d",
+                padding: "0.5em",
+                marginTop: "10px",
+                fontWeight: "500"
+              }
+            }
             type="password"
-            value={this.state.password}
+            value={value}
             name="input-text"
             label="password"
             placeholder="Password"
-            onChange={(e) => this.handleUserPasswordInput(e)}
+            onChange={onChange}
+            onBlur={onBlur}
             />
         </div>
     )
   }
 }
 
-const InputStyle = {
-  boxSizing: "border-box",
-  width: "100%",
-  height: "30px",
-  background: "none",
-  borderRadius: 0,
-  border: 0,
-  borderBottom: "1px solid #e9ecef",
-  color: "#6c757d",
-  padding: "0.5em",
-  marginTop: "10px",
-  fontWeight: "500"
-}
 export default InputPassword;
