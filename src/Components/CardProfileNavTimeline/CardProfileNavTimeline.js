28339e83f1bb47042f98e330c5b74ca4e6b36522
bluesophia <imsophia0313@gmail.com>
2019-03-01 11:41:28 +1300

    add comment

--- src/Components/CardProfileNavTimeline/CardProfileNavTimeline.js
+++ src/Components/CardProfileNavTimeline/CardProfileNavTimeline.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './CardProfileNavTimeline.css';
 import ImgCircle from '../Common/ImgCircle/ImgCircle';
-import SimpleLineIcon from 'react-simple-line-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { RectangleButton } from '../Common/Button/ButtonExports';
@@ -9,49 +8,60 @@ import { RectangleButton } from '../Common/Button/ButtonExports';
 class CardProfileNavTimeline extends Component {
     render(){
 
-        const { name, imgUrl, image1, image2, image3, image4, update, updateWhere, comment, commentNumber, like, time, button, Blockquote  } = this.props;
+        const { name, imgUrl, image1, image2, image3, image4, update, updateWhere, 
+            comment, commentNumber, like, time, button, Blockquote  } = this.props;
         
         return(
             <div className="profile-timeline-row">
                 <div className="profile-timeline">
-                <div className="profile-timeline-left-image">
-                    <span> 
-                        <ImgCircle src={imgUrl} width={"40px"}/>
-                    </span>
-                </div>
-                <div className="profile-timeline-text">
-                    <div>
-                        <a href="#">{name}</a>
-                        <span className="profile-timeline-date">{time}</span>
-                        { update ? <p className="profile-timeline-text-update">{update}<a href="#" className="profile-timeline-updateWhere">  {updateWhere}</a></p> : null }
+
+                    {/* timeline profile image */}
+                    <div className="profile-timeline-left-image">
+                        <span> 
+                            <ImgCircle src={imgUrl} width={"40px"}/>
+                        </span>
                     </div>
-                    <div className="profile-timeline-contents">
-                        <div className="profile-timeline-image-row">
-                            { image1 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image1}/></div> : null }
-                            { image2 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image2}/></div> : null }
-                            { image3 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image3}/></div> : null }
-                            { image4 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image4}/></div> : null }
-                            <div className="profile-timeline-comments-right">
-                            { comment ? <p className="profile-timeline-comment">{comment}</p> : null }
-                            { button ? 
-                            <RectangleButton 
-                                marginBottom={"20px"}
-                                backgroundColor="var(--teal)"
-                                backgroundColorHover="#009c75"
-                                color="#fff"
-                                hoverColor="#fff"
-                            /> 
-                            :
-                             null 
-                            }
+
+                    {/* update person */}
+                    <div className="profile-timeline-text">
+                        <div>
+                            <a href="#">{name}</a>
+                            <span className="profile-timeline-date">{time}</span>
+                            { update ? <p className="profile-timeline-text-update">{update}<a href="#" className="profile-timeline-updateWhere">  {updateWhere}</a></p> : null }
+                        </div>
+
+                        {/* images */}
+                        <div className="profile-timeline-contents">
+                            <div className="profile-timeline-image-row">
+                                { image1 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image1}/></div> : null }
+                                { image2 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image2}/></div> : null }
+                                { image3 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image3}/></div> : null }
+                                { image4 ? <div className="profile-timeline-img-div col-lg-3 col-md-6"><img className="profile-timeline-img" src={image4}/></div> : null }
+                                <div className="profile-timeline-comments-right">
+                                { comment ? <p className="profile-timeline-comment">{comment}</p> : null }
+                                { button ? 
+                                <RectangleButton 
+                                    marginBottom={"20px"}
+                                    backgroundColor="var(--teal)"
+                                    backgroundColorHover="#009c75"
+                                    color="#fff"
+                                    hoverColor="#fff"
+                                /> 
+                                :
+                                null 
+                                }
                             </div>
                         </div>
+
+                        {/* blackqoute */}
                         <div className="profile-timeline-comment-div col-lg-3 col-md-6">
                             
                             { Blockquote ? <p className="profile-timeline-blockquote">{Blockquote}</p> : null }
                             
                         </div>
                     </div>
+
+                    {/* footer-comment */}
                     <div className="profile-timeline-footer">
                         <span className="profile-timeline-action-icons">
                             { commentNumber ? <a herf="#"><p>{commentNumber} comment</p></a> : null }
@@ -63,6 +73,7 @@ class CardProfileNavTimeline extends Component {
                             : null }
                         </span>
                     </div>
+
                 </div>
                     <hr className="profile-timeline-hr"/>
                 </div>
@@ -70,6 +81,7 @@ class CardProfileNavTimeline extends Component {
         )
     }
 };
+
 // FontAwesomeIconStyle
 const FontAwesomeIconStyle = {
     fontSize: '14px',
